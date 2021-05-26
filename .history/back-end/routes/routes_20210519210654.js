1e983bc5b684991a9ec7ab9d0d4c521f9506ba9e
Dang Quang <63301530+1612538@users.noreply.github.com>
2021-05-27 03:30:43 +0700

    Delete .history directory

--- .history/back-end/routes/routes_20210519210654.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-module.exports = (app) => {
-  //Courses controller
-  let Courses = require("./controllers/CoursesController");
-};
--- .history/back-end/routes/routes_20210519211559.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-module.exports = (app) => {
-  //Courses controller
-
-  let Courses = require("./controllers/CoursesController");
-  app.route("/api/courses").get(Courses.get).post(Courses.add).delete();
-};
